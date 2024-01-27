d5628b4a135fe70d72f20b672d14201e14e5369c
Rack e <tim@gmail.com>
2024-01-26 22:17:32 -0500

    Finished ConvertToOctal method

--- src/ConverterRunner.java
+++ src/ConverterRunner.java
@@ -33,6 +33,12 @@ class ConverterRunner {
         int[] digits = nc.getDigits();
         System.out.println("\n\nDigit array: " + Arrays.toString(digits));
         System.out.println("Number: " + nc.displayOriginalNumber());
-        System.out.println("Decimal: "+ Arrays.toString(nc.convertToDecimal()));
+        if(base==8){
+        System.out.println("Decimal: "+ Arrays.toString(nc.convertToDecimal()));}
+        else if(base==10){
+        System.out.println("Octal: "+Arrays.toString(nc.convertToOctal()));}
+        else {
+
+        }
     }
 }
--- src/NumberConverter.java
+++ src/NumberConverter.java
@@ -32,7 +32,7 @@ public class NumberConverter {
         int[] sum = new int[1];
         int y =0;
         for(int i=digits.length-1;i>=0;i--){
-            sum[0]+= digits[i]*Math.pow(base,y);
+            sum[0]+= (int) (digits[i]*Math.pow(base,y));
             y++;
         }
         return sum;}
@@ -43,8 +43,37 @@ public class NumberConverter {
 
     public int[] convertToOctal() {
         String notReversed="";
-        int quotient=Integer.parseInt(Arrays.toString(digits));
+        String quotient="";
+        int realQuotient=0;
+        if(base==2){
+            int[] sum = new int[1];
+            int y =0;
+            for(int i=digits.length-1;i>=0;i--){
+                sum[0]+= (int) (digits[i]*Math.pow(base,y));
+                y++;
+            }
+            realQuotient=sum[0];
+        }
+        else {
+            for (int i = 0; i < digits.length; i++) {
+                quotient += Integer.toString(digits[i]);
+            }
+            realQuotient = Integer.parseInt(quotient);
+        }
+        while(realQuotient != 0){
+            notReversed+=Integer.toString(realQuotient%8);
+            realQuotient=realQuotient/8;
+        }
 
+        String correctOrder="";
+    for(int i=notReversed.length();i>0;i--){
+        correctOrder+=notReversed.substring(i-1,i);
+
+    }
+    int[] number =new int[1];
+    int correctNumber=Integer.parseInt(correctOrder);
+    number[0]=correctNumber;
+    return number;
     }
 }
 
